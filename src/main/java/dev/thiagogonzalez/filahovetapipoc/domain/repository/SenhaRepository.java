9b8c216b3cb45f3e27fa8dcb0f8b23a085eaf6f2
thiago.gonzalez <thiagogonzalez.dev@gmail.com>
2025-01-16 03:25:37 -0300

    chore: removendo mock das queries

--- src/main/java/dev/thiagogonzalez/filahovetapipoc/domain/repository/SenhaRepository.java
+++ src/main/java/dev/thiagogonzalez/filahovetapipoc/domain/repository/SenhaRepository.java
@@ -11,14 +11,14 @@ import java.util.List;
 
 @Repository
 public interface SenhaRepository extends JpaRepository<Senha, Long> {
-    @Query("SELECT s FROM Senha s WHERE s.situacao = 'PENDENTE_ATENDIMENTO' AND s.fila.id = 1 order by s.dataCriacao limit 1")
-    public Senha findSenhaAtualByIdFila(Long idFila);
+    @Query("SELECT s FROM Senha s WHERE s.situacao = 'PENDENTE_ATENDIMENTO' AND s.fila.id = :idFila order by s.dataCriacao limit 1")
+    public Senha findSenhaAtualByIdFila(@Param("idFila") Long idFila);
 
     @Query("SELECT s FROM Senha s WHERE s.situacao = 'PENDENTE_ATENDIMENTO' AND s.fila.id = :idFila and s.id <> :idSenhaAtual  order by s.dataCriacao")
     public List<Senha> findProximasSenhas(@Param("idFila") Long idFila, @Param("idSenhaAtual") Long idSenhaAtual);
 
-    @Query("SELECT s FROM Senha s WHERE s.situacao = 'ATENDIDA' AND s.fila.id = 1 order by s.ordem DESC")
-    public List<Senha> findSenhasAtendidasByIdFila(Long filaId);
+    @Query("SELECT s FROM Senha s WHERE s.situacao = 'ATENDIDA' AND s.fila.id = :idFila order by s.ordem DESC")
+    public List<Senha> findSenhasAtendidasByIdFila(@Param("idFila") Long idFila);
 
     @Query("SELECT s FROM Senha s WHERE DATE(s.dataCriacao) = DATE(:date) AND s.fila.id = :filaId")
     public List<Senha> queryFindByDate(@Param("date") String date, @Param("filaId") Long filaId);
